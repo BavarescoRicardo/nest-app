94bff32dd2a69c023c793f86373f57ef7414284a
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-04-07 00:37:08 -0300

    added pipe validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,7 @@
+import { MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @MinLength(5, { message: "The user name should be longer than 5 letters"})
     name: String;
     last_name: String;
     hash: String;
