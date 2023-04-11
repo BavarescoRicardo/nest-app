6a529758d179395c0ad8e2873838af9f7679055a
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-04-11 00:06:44 -0300

    added mock to override provider

--- src/email/email.service.spec.ts
+++ src/email/email.service.spec.ts
@@ -1,13 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmailService } from './email.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 describe('EmailService', () => {
   let service: EmailService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const mockEmail = {}
+    const module: TestingModule = await Test.createTestingModule({      
       providers: [EmailService],
-    }).compile();
+      exports: [EmailService]
+    }).overrideProvider(EmailService).useValue(mockEmail).compile();
 
     service = module.get<EmailService>(EmailService);
   });
