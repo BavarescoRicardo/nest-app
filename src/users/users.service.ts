bc4345a464abd9d32de0325f801e5d6e4af802bb
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-04-10 22:33:53 -0300

    clean code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,8 +5,6 @@ import { User } from './entities/user.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDto } from './dto/user.dto';
-import { MessageBrokerService } from 'src/message-broker/message-broker.service';
-
 
 @Injectable()
 export class UsersService {
