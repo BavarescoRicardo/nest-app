1d785ab0ec6093dc5ed1bfaae96fa3bb4849668c
BavarescoRicardo <ricardo.bav17@gmail.com>
2023-04-14 11:31:07 -0300

    class validator for dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,11 @@
-import { MinLength } from "class-validator";
+import { IsNotEmpty, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @MinLength(5, { message: "The user name should be longer than 5 letters"})
     email: String;
+    @IsNotEmpty()
     first_name: String;
+    @IsNotEmpty()
     last_name: String;
     avatar: String;
 }
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,6 +1,9 @@
+import { IsNotEmpty } from "class-validator";
 
 export class UserDto {
+    @IsNotEmpty()
     email: String;
+    @IsNotEmpty()
     last_name: String;
     avatar: String;
 }
